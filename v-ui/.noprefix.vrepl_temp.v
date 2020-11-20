0e3de8a048b9a7a1496e0d98daabf3df9d8951b4
hirossan4049 <haruto405329@gmail.com>
2020-11-20 21:15:00 +0900

    update

---
+++ v-ui/.noprefix.vrepl_temp.v
@@ -0,0 +1,12 @@
+import os
+
+import time
+
+import math
+
+for i in 19..27{
+
+println(i)
+
+}
+
--- v-ui/test.v
+++ v-ui/test.v
@@ -43,7 +43,7 @@ fn btn_count_click(mut app App, btn &ui.Button) {
 fn canvas_draw(gg &gg.Context, app &App) {
     //gg.draw_circle(100, 300, 100, gx.red)
     //gg.draw_circle_with_segments(100, 300, 200, 20, gx.green)
-    gg.draw_rect(100,100,200,200, gx.red)
+    gg.draw_rect(50,50,100,100, gx.green)
     //gg.draw_circle_with_segments(44,24,14,64,gx.red)
 
     //sgl.c4b(255, 0,0,255)
@@ -61,18 +61,19 @@ fn canvas_draw(gg &gg.Context, app &App) {
     ////sgl.v2f(30,30)
     //sgl.end()
 
-	sgl.c4b(98,0,238,255)
+	//sgl.c4b(98,0,238,255)
+	sgl.c4b(255,0,0,255)
 	mut theta := f32(0)
 	mut xx:= f32(0)
 	mut yy:= f32(0)
-	r := 10
-	x := (100 + r/2) * gg.scale
-	y := (100 + r/2) * gg.scale
+	r := 25
+	x := 100
+	y := 100
 	//width := (200  - int(any_float(r)*2.75)) * gg.scale
 	//height := (200  - int(any_float(r)*2.75)) * gg.scale
-	width := 200 + 100 + r / 3
-	height := 200 + 100 + r / 3
-	segments := f32(30)
+	width := 100
+	height := 100
+	segments := f32(36)
                            
 	//if r > x - r + int(segments) || r > y - r + int(segments){
 	//	gg.draw_circle_with_segments(x,y,r,30,gx.blue)
@@ -84,89 +85,91 @@ fn canvas_draw(gg &gg.Context, app &App) {
 	//theta = 2 * f32(math.pi) * 14 / segments
 	//xx = r * math.cosf(theta)
     // left top 9
-	for i in 15..24{
+	lx := x + r
+	ly := y + r
+	for i in 18..27{
 		theta = 2 * f32(math.pi) * f32(i) / segments
 		xx = r * math.cosf(theta)
 		yy = r * math.sinf(theta)
-		sgl.v2f(xx + x,yy + y)
+		sgl.v2f(xx + lx,yy + ly)
 	}
-	sgl.v2f(x+width, yy + y)
+	//sgl.v2f(x+width, yy + y)
 
 	// right top 6
-	mut rx := x+xx+width-r/3
-	mut ry := yy + y + r
-	for i in 24..30{
+	mut rx := x + 2*width - r
+	mut ry := y + r
+	for i in 27..36{
 		theta = 2 * f32(math.pi) * f32(i) / segments
 		xx = r * math.cosf(theta)
 		yy = r * math.sinf(theta)
 		sgl.v2f(xx + rx,yy + ry)
 	}
 
-	sgl.v2f(xx+rx,y + height-r/2)
+	//sgl.v2f(xx+rx,y + height-r/2)
 
 	// right bottom 8
 	mut rbx := rx
-	mut rby := ry + height - r/ 3
-	for i in 31..39{
+	mut rby := y + 2*height - r
+	for i in 1..9{
 		theta = 2 * f32(math.pi) * f32(i) / segments
 		xx = r * math.cosf(theta)
 		yy = r * math.sinf(theta)
 		sgl.v2f(xx + rbx,yy + rby)
 	}
-	sgl.v2f(xx + x,rby+yy)
+	//sgl.v2f(xx + x,rby+yy)
 
 	// 7
-	mut lx := x
-	mut ly := ry + height - r/ 3
-	for i in 39..45{
+	mut lbx := lx
+	mut lby := y + 2*height - r
+	for i in 10..18{
 		theta = 2 * f32(math.pi) * f32(i) / segments
 		xx = r * math.cosf(theta)
 		yy = r * math.sinf(theta)
-		sgl.v2f(xx + lx,yy + ly)
+		sgl.v2f(xx + lbx,yy + lby)
 	}
-	sgl.v2f(x + xx, y)
+	sgl.v2f(lx + xx, ly)
 
 	sgl.end()
 
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!1
 	//sgl.c4b(98,0,238,255)
-	sgl.c4b(25,0,0,255)
-	sgl.begin_line_strip()
-	for i in 15..24{
-		theta = 2 * f32(math.pi) * f32(i) / segments
-		xx = r * math.cosf(theta)
-		yy = r * math.sinf(theta)
-		sgl.v2f(xx + x+1,yy + y+1)
-	}
-	sgl.v2f(x+width, yy + y + 1)
-	rx = x+xx+width-r/3
-	ry = yy + y + r
-	for i in 24..30{
-		theta = 2 * f32(math.pi) * f32(i) / segments
-		xx = r * math.cosf(theta)
-		yy = r * math.sinf(theta)
-		sgl.v2f(xx + rx - 1,yy + ry + 1)
-	}
-	sgl.v2f(xx+rx - 1,y + height-r/2 - 1)
-	rbx = rx
-	rby = ry + height - r/ 3
-	for i in 31..39{
-		theta = 2 * f32(math.pi) * f32(i) / segments
-		xx = r * math.cosf(theta)
-		yy = r * math.sinf(theta)
-		sgl.v2f(xx + rbx - 1,yy + rby - 1)
-	}
-	sgl.v2f(xx + x - 1,rby+yy - 1)
-	lx = x
-	ly = ry + height - r/ 3
-	for i in 39..45{
-		theta = 2 * f32(math.pi) * f32(i) / segments
-		xx = r * math.cosf(theta)
-		yy = r * math.sinf(theta)
-		sgl.v2f(xx + lx + 1,yy + ly - 1)
-	}
-	sgl.v2f(x + xx + 1, y + 1)
-	sgl.end()
+	//sgl.c4b(25,0,0,255)
+	//sgl.begin_line_strip()
+	//for i in 15..24{
+	//	theta = 2 * f32(math.pi) * f32(i) / segments
+	//	xx = r * math.cosf(theta)
+	//	yy = r * math.sinf(theta)
+	//	sgl.v2f(xx + x+1,yy + y+1)
+	//}
+	//sgl.v2f(x+width, yy + y + 1)
+	//rx = x+xx+width-r/3
+	//ry = yy + y + r
+	//for i in 24..30{
+	//	theta = 2 * f32(math.pi) * f32(i) / segments
+	//	xx = r * math.cosf(theta)
+	//	yy = r * math.sinf(theta)
+	//	sgl.v2f(xx + rx - 1,yy + ry + 1)
+	//}
+	//sgl.v2f(xx+rx - 1,y + height-r/2 - 1)
+	//rbx = rx
+	//rby = ry + height - r/ 3
+	//for i in 31..39{
+	//	theta = 2 * f32(math.pi) * f32(i) / segments
+	//	xx = r * math.cosf(theta)
+	//	yy = r * math.sinf(theta)
+	//	sgl.v2f(xx + rbx - 1,yy + rby - 1)
+	//}
+	//sgl.v2f(xx + x - 1,rby+yy - 1)
+	//lx = x
+	//ly = ry + height - r/ 3
+	//for i in 39..45{
+	//	theta = 2 * f32(math.pi) * f32(i) / segments
+	//	xx = r * math.cosf(theta)
+	//	yy = r * math.sinf(theta)
+	//	sgl.v2f(xx + lx + 1,yy + ly - 1)
+	//}
+	//sgl.v2f(x + xx + 1, y + 1)
+	//sgl.end()
 	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	//btn_cfg := gx.TextCfg{
